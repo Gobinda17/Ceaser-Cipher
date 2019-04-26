d33a6e84f049d38204bd2f6d75ad1069f7df7a6f
Gobinda Deb <gobindadeb14@gmail.com>
2019-04-26 13:48:03 +0530

    remove cipher.py cipher2.py key.txt max.py sets.py test.py test2.py test3.py test4.py tk.py tuple.py

--- test2.py
+++
@@ -1,8 +0,0 @@
-print ("Enter three numbers:")
-x = int(raw_input())
-y = int(raw_input())
-z = int(raw_input())
-
-s = x + y + z
-print (type(s))
-#print ("The sum is: " +s)
--- tuple.py
+++
@@ -1,8 +0,0 @@
-x = tuple(("apple", "banana", "mango"))
-#print (type(x))
-#x[2] = "orange"
-print ("Entre your string:")
-y = input()
-print (y)
-for y in x:
-    print (y)
